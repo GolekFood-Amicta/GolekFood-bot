59f982a3ca304409b561feac87ca203846cef5e5
ARIFIN YUNIANTA <arifinklaten26@gmail.com>
2023-07-05 14:17:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const qrcode = require('qrcode-terminal');
 
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const puppeteer = require('puppeteer');
+
+puppeteer.launch({ executablePath: '/path/to/chrome' });
+
+const { Client, LocalAuth, MessageMedia, AuthStrategy } = require('whatsapp-web.js');
+const client = new Client({
+    AuthStrategy: new LocalAuth(),
+    puppeter: {headless : false}
+});
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
@@ -16,7 +23,14 @@ client.on('message', message => {
 		message.reply('pong');
 	}
 });
- 
+
+client.on('message', message =>{
+    if(message.body === '!help'){
+        message.reply('konsol');
+    }
+
+});
+
 
 client.initialize();
  
